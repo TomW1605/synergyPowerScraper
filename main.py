96ce645622f3903f2d642e4880e4dd0f8aab3b26
Thomas White <TomW1605@users.noreply.github.com>
2025-02-20 19:31:04 +0800

    convert to using argparse

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
+import argparse
 import datetime
-import sys
 
 from SynergyDataFetcher import SynergyDataFetcher
 
-if len(sys.argv) < 6:
-    print("Usage: python script.py <premise_id> <email_address> <password> <email_server> <email_port>")
-    sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(description="Fetch and parse Synergy usage data.")
+    parser.add_argument("--premise_id", type=str, help="Premise ID", required=True)
+    parser.add_argument('-e', "--email_address", type=str, help="Email address", required=True)
+    parser.add_argument('-p', "--password", type=str, help="Password", required=True)
+    parser.add_argument('-s', "--email_server", type=str, help="Email server", required=True)
+    parser.add_argument("--email_port", type=int, help="Email port", default=993)
 
-premise_id = sys.argv[1]
-email_address = sys.argv[2]
-password = sys.argv[3]
-email_server = sys.argv[4]
+    args = parser.parse_args()
 
-try:
-    email_port = int(sys.argv[5])
-except ValueError:
-    print("Error: email_port must be a non-empty integer.")
-    sys.exit(1)
+    synergy_fetcher = SynergyDataFetcher(args.premise_id, args.email_address, args.password, args.email_server, args.email_port)
+    raw_usage_data = synergy_fetcher.fetch(datetime.date.today() - datetime.timedelta(days=4), datetime.date.today())
+    parsed_usage_data = synergy_fetcher.parse()
+    print(parsed_usage_data)
 
-# Perform type and existence checks
-if not premise_id or not isinstance(premise_id, str):
-    print("Error: premise_id must be a non-empty string.")
-    sys.exit(1)
-
-if not email_address or not isinstance(email_address, str):
-    print("Error: email_address must be a non-empty string.")
-    sys.exit(1)
-
-if not password or not isinstance(password, str):
-    print("Error: password must be a non-empty string.")
-    sys.exit(1)
-
-if not email_server or not isinstance(email_server, str):
-    print("Error: email_server must be a non-empty string.")
-    sys.exit(1)
-
-if not email_port or not isinstance(email_port, int):
-    print("Error: email_port must be a non-empty integer.")
-    sys.exit(1)
-
-# from test_data import usage_data
-synergy_fetcher = SynergyDataFetcher(premise_id, email_address, password, email_server, email_port) #, usage_data)
-raw_usage_data = synergy_fetcher.fetch(datetime.date.today() - datetime.timedelta(days=4), datetime.date.today())
-parsed_usage_data = synergy_fetcher.parse()
-print(parsed_usage_data)
+if __name__ == "__main__":
+    main()
